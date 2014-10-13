9b3b50200b45b5f94327907bca6df01fcce16b6f
avijaykumar40 <avijaykumar40@gmail.com>
2014-10-13 17:21:30 +0530

    Added Units Services

--- src/main/java/com/demo/jerssy/JerseyClientUnits.java
+++ src/main/java/com/demo/jerssy/JerseyClientUnits.java
@@ -11,10 +11,10 @@ public class JerseyClientUnits {
 		try {
 			Client client = Client.create();
 			WebResource webResource = client
-					.resource("http://localhost:8080/Shopping/rest/unitService/insertUnit");
-			String input = "{\"unitName\":\"unitName8\"}";
+					.resource("http://localhost:8080/Shopping/rest/unitService/insertg");
+			String input = "{\"unitName\":\"10GMS\"}";
 			ClientResponse response = webResource.type("application/json")
-					.post(ClientResponse.class, input);
+					.post(ClientResponse.class,input);
 
 			if (response.getStatus() != 201) {
 				throw new RuntimeException("Failed : HTTP error code : "
@@ -34,3 +34,39 @@ public class JerseyClientUnits {
 	}
 
 }
+/*
+	GET CHECK
+	=========
+	http://localhost:8080/Shopping/rest/unitService/get
+	
+	POST CHECK
+	==========
+	WebResource webResource = client.resource("http://localhost:8080/Shopping/rest/unitService/post");
+	String input = "{\"unitName\":\"GMS\"}";
+	
+	Insert
+	======
+	WebResource webResource = client.resource("http://localhost:8080/Shopping/rest/unitService/insert");
+	String input = "{\"unitName\":\"GMS\"}";
+	
+	O/P ---> {"unitName":"GMS","createdBy":0,"modifiedBy":0,"statusMsg":"sucess","id":10}
+
+	
+	Update
+	======
+	WebResource webResource = client.resource("http://localhost:8080/Shopping/rest/unitService/insert");
+	String input = "{\"unitName\":\"GMS\",\"id\":\"10\"}";
+	
+	O/P ---> {"unitName":"GMS","createdBy":0,"modifiedBy":0,"statusMsg":"sucess","id":10}
+	
+	GET LIST
+	========
+	WebResource webResource = client.resource("http://localhost:8080/Shopping/rest/unitService/getAll");
+	//String input = "{\"unitName\":\"GMS\"}";
+	
+	O/P --->
+	[{"unitName":"GMS","createdBy":0,"modifiedBy":0,"statusMsg":null,"id":10},
+	{"unitName":"KGS","createdBy":0,"modifiedBy":0,"statusMsg":null,"id":20},
+	{"unitName":"LTR","createdBy":0,"modifiedBy":0,"statusMsg":null,"id":21},
+	{"unitName":"10GMS","createdBy":0,"modifiedBy":0,"statusMsg":null,"id":22}]
+*/
--- src/main/java/com/shopping/action/Units.java
+++ src/main/java/com/shopping/action/Units.java
@@ -1,5 +1,8 @@
 package com.shopping.action;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -16,6 +19,7 @@ import com.shopping.to.UnitsTo;
 
 @Path("/unitService")
 public class Units {
+	// GET TESTING
 	@GET
 	@Path("/get")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -29,6 +33,7 @@ public class Units {
 		return unitTo;
 	}
 
+	// POST TESTING
 	@POST
 	@Path("/post")
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -38,10 +43,11 @@ public class Units {
 		
 	}
 	
+	// UNIT INSERT
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-	@Path("/insertUnit") 
+	@Path("/insert") 
 	public Response insertUnits(UnitsTo unitTo,@Context HttpServletRequest request){
 		try 
 		{
@@ -55,8 +61,51 @@ public class Units {
 		e.printStackTrace();
 		String error = "ConstraintViolationException";
 		unitTo.setStatusMsg(error);
-		return Response.status(201).entity(error).build();
+		return Response.status(403).entity(error).build();
 		}
 	}
+	
+	// UNIT UPDATE
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+	@Path("/update") 
+	public Response updateUnits(UnitsTo unitTo,@Context HttpServletRequest request){
+		try 
+		{
+			unitTo=ShoppingCartFactory.getUnitsDao().update(unitTo.getId(), unitTo);
+			unitTo.setStatusMsg("sucess");
+						
+			return Response.status(201).entity(unitTo).build();	
+		}
+		catch (Exception e)
+		{
+		e.printStackTrace();
+		String error = "ConstraintViolationException";
+		unitTo.setStatusMsg(error);
+		return Response.status(403).entity(error).build();
+		}
+	}
+	// UNITS GET ALL
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+	@Path("/getAll") 
+	public Response getAllUnits(@Context HttpServletRequest request){
+		try 
+		{
+			Collection<UnitsTo> unitTo = new ArrayList<UnitsTo>();
+			unitTo=ShoppingCartFactory.getUnitsDao().getAll();
+						
+			return Response.status(201).entity(unitTo).build();	
+		}
+		catch (Exception e)
+		{
+		e.printStackTrace();
+		String error = "ConstraintViolationException";
+		return Response.status(403).entity(error).build();
+		}
+	}
+	
 
 }
