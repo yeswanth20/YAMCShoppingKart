a827af3d7313dae54df54b596ae9db7fae826ae3
unknown <yes.yeswanth20@gmail.com>
2014-11-03 22:54:07 +0530

    admin panel

--- src/main/webapp/admin/controllers/products-controller.js
+++ src/main/webapp/admin/controllers/products-controller.js
@@ -1,76 +1,94 @@
 angular.module("shopApp").controller("productsController",
-	["$scope",
-	function($scope){
-		$scope.data = [
-		  {
-		    "id": 1,
-		    "title": "node1",
-		    "nodes": [
-		      {
-		        "id": 11,
-		        "title": "node1.1",
-		        "nodes": [
-		          {
-		            "id": 111,
-		            "title": "node1.1.1",
-		            "nodes": []
-		          }
-		        ]
-		      },
-		      {
-		        "id": 12,
-		        "title": "node1.2",
-		        "nodes": []
-		      }
-		    ]
-		  },
-		  {
-		    "id": 2,
-		    "title": "node2",
-		    "nodes": [
-		      {
-		        "id": 21,
-		        "title": "node2.1",
-		        "nodes": []
-		      },
-		      {
-		        "id": 22,
-		        "title": "node2.2",
-		        "nodes": []
-		      }
-		    ]
-		  },
-		  {
-		    "id": 3,
-		    "title": "node3",
-		    "nodes": [
-		      {
-		        "id": 31,
-		        "title": "node3.1",
-		        "nodes": []
-		      }
-		    ]
-		  },
-		  {
-		    "id": 4,
-		    "title": "node4",
-		    "nodes": [
-		      {
-		        "id": 41,
-		        "title": "node4.1",
-		        "nodes": []
-		      }
-		    ]
-		  }
-		];	
+	["$scope","categoriesService","unitsService","weightsService","brandsService",
+	function($scope,categoriesService,unitsService,weightsService,brandsService){
 
-		$scope.newSubItem = function(scope) {
-	      var nodeData = scope.$modelValue;
-	      nodeData.nodes.push({
-	        id: nodeData.id * 10 + nodeData.nodes.length,
-	        title: nodeData.title + '.' + (nodeData.nodes.length + 1),
-	        items: []
-	      });
-	    };
+		$scope.productFormData = {
+			"units" : [],
+			"units" : [],
+		};	
+
+		$scope.initializeCategories = function() {
+			var finalCategoriesList = [];
+			categoriesService.getCategories().then(function(categoriesList){			
+				var categoriesCounter = 1;
+				$scope.originalCategoriesList = categoriesList;
+				if(categoriesList.length==0) {
+					$scope.showCategoryFormFlag = true;
+				}
+
+				var rootCateogies  = _.where(categoriesList, function(rw){
+					return rw.id == rw.parentId;
+				});
+
+				for(var rootIndex in rootCateogies) {
+					finalCategoriesList[rootIndex] = {};
+					finalCategoriesList[rootIndex].id = rootCateogies[rootIndex].id;
+					finalCategoriesList[rootIndex].name = rootCateogies[rootIndex].categoryNameEng;
+					finalCategoriesList[rootIndex].nodes = $scope.buildRecursiveTree(rootCateogies[rootIndex].id,categoriesList);
+				}			
+				$scope.categoriesList = finalCategoriesList;
+			});
+		};
+
+		$scope.initializeCategories();
+
+		$scope.buildRecursiveTree = function(parentId,categoriesList) {
+
+			var rootCateogies  = _.where(categoriesList, function(rw){
+				return rw.parentId == parentId && rw.id!=parentId;
+			});
+
+			if(rootCateogies.length>0)
+			{
+				var tempCategoriesList = [];
+				for(var rootIndex in rootCateogies) {
+					tempCategoriesList[rootIndex] = {};
+					tempCategoriesList[rootIndex].id = rootCateogies[rootIndex].id;
+					tempCategoriesList[rootIndex].name = rootCateogies[rootIndex].categoryNameEng;
+					tempCategoriesList[rootIndex].nodes = $scope.buildRecursiveTree(rootCateogies[rootIndex].id,categoriesList);
+				}
+				return tempCategoriesList;
+			}
+			else
+			{
+				return [];
+			}
+
+		};
+		
+		unitsService.getUnits().then(function(result){
+			$scope.unitsList = result;
+		});
+
+		weightsService.getWeights().then(function(result){
+			$scope.weightsList = result;
+		});
+
+		brandsService.getBrands().then(function(result){
+			$scope.brandsList = result;
+		});
+		
+		$scope.unitsRows = [];
+
+		$scope.unitsRows[$scope.unitsRows.length] = {
+			"name" : "Abc"
+		};
+
+		$scope.addUnitsRows = function(){			
+			$scope.unitsRows[$scope.unitsRows.length] = {
+				"name" : "Xyz"
+			};
+		};
+
+		$scope.removeUnitRow = function(id) {
+			$scope.unitsRows.splice(id,1);		
+		}
+
+		$scope.createProduct = function() {
+
+
+
+		};
+
 	}
 ]);
