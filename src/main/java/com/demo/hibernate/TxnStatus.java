9194a7eb5b7fa2d196c31e4b8aaa8a926b0cd52e
Khudrath <bigc@khudrath>
2014-11-06 12:58:27 +0530

    txn status & city test

---
+++ src/main/java/com/demo/hibernate/TxnStatus.java
@@ -0,0 +1,19 @@
+package com.demo.hibernate;
+
+import com.shopping.daoimpl.TransactionStatusDaoimpl;
+import com.shopping.to.TransactionStatusTo;
+
+public class TxnStatus {
+	
+	public static void main(String[] args) {
+		TransactionStatusTo txnStatusTo = new TransactionStatusTo();
+		txnStatusTo.setStatusName("Status 2");
+		TransactionStatusDaoimpl txnStatusDaoimpl = new TransactionStatusDaoimpl();
+		txnStatusDaoimpl.insert(txnStatusTo,1);
+		txnStatusDaoimpl.getAll();
+		
+	}
+
+}
+
+
