733140b8d59e653fbb2b55224e02c202c15411c3
pradeep <pradeep@pradeep-pc>
2014-11-07 11:24:39 +0530

    Product in Pagenation

--- src/main/java/com/shopping/dao/ProductDao.java
+++ src/main/java/com/shopping/dao/ProductDao.java
@@ -9,7 +9,7 @@ public interface ProductDao {
 	public ProductTo insert(ProductTo productTo, int userId);
 	public ProductTo update(int id, ProductTo productTo, int userId);
 	public ProductTo searchById(int id);
-	public Collection<ProductTo> getAll();
+	public Collection<ProductTo> getAll(int pageNumber,int pageSize);
 	public ProductOrm getProductById(int id);
 	
 	public boolean delete(int id);
