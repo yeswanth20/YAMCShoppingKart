241fa49ee9fc6146c9d39d2fa0726ae374efa125
yeswanth20 <yes.yeswanth20@gmail.com>
2014-12-13 02:12:04 +0530

    ui

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -27,6 +27,34 @@ angular.module("shopApp",
             data : {
                 action : "cart",
             }
+        })
+        .state('login', {
+            url: "/login",
+            templateUrl: themeBasePath+'/Login.html',
+            data : {
+                action : "login",
+            }
+        })
+        .state('myaccounts', {
+            url: "/myaccounts",
+            templateUrl: themeBasePath+'/Myaccount.html',
+            data : {
+                action : "Myaccount",
+            }
+        })
+        .state('productview', {
+            url: "/productview",
+            templateUrl: themeBasePath+'/product_view.html',
+            data : {
+                action : "productview",
+            }
+        })  
+        .state('orderconformation', {
+            url: "/orderconformation",
+            templateUrl: themeBasePath+'/orderconformation.html',
+            data : {
+                action : "orderconformation",
+            }
         })        
 
         $urlRouterProvider.otherwise("home");
