e2b81e02c9c7a934e62a91d7998045a1aad6d621
avijaykumar40 <avijaykumar40@avijaykumar40-pc>
2014-12-15 14:21:38 +0530

    Changed Filter Configuration

--- src/main/java/com/shopping/action/AuthService.java
+++ src/main/java/com/shopping/action/AuthService.java
@@ -1,52 +1,38 @@
 package com.shopping.action;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.net.URI;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import com.shopping.daofactory.ShoppingCartFactory;
-import com.shopping.to.ProductTo;
-import com.shopping.to.UsersTo;
 
 @Path("/accessService")
 public class AuthService {
 	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/login")
-	public Response loginAuth(UsersTo usersTo,
-			@Context HttpServletRequest request) {
+	public Response loginAuth(@FormParam("email") String username,@FormParam("password") String password,
+			@Context HttpServletRequest request, @Context UriInfo uriInfo) {
 		int userId;
 		try {
-			userId = ShoppingCartFactory.getUserDao().verifyLogin(
-					usersTo.getUserName(), usersTo.getPassword());
+			userId = ShoppingCartFactory.getUserDao().verifyLogin(username, password);
 			if (userId != 0) {
 				request.setAttribute("userId", userId);
 				HttpSession httpsession = request.getSession();
 				httpsession.setAttribute("ipaddress", request.getRemoteHost());
-				
-				Collection<ProductTo> productTo = new ArrayList<ProductTo>();
-				int pageNumber=1;
-				int pageSize=10;
-				productTo = ShoppingCartFactory.getProductDao().getAll(pageNumber, pageSize);
-				return Response.status(201).entity(productTo).build();
-			}
-//				return Response.status(201).entity("sucess"+userId).build();
-//				servletResponse.sendRedirect("http://localhost:8080/MySite/pages/Create_Profile.html");			}
-				
-//				URI uri = uriInfo.getBaseUriBuilder().path("../user.html").build();
-//				return Response.seeOther(uri).build();
-				else{
-				return Response.status(201).entity("Invalid Login").build();
+				URI uri = uriInfo.getBaseUriBuilder().path("../admin").build();
+				return Response.seeOther(uri).build();
+			}else{
+				URI uri = uriInfo.getBaseUriBuilder().path("../admin/login.html").build();
+				return Response.seeOther(uri).build();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
