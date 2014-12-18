9bb1b70337918aeb67b4d2f434c6aedfcb7e24dc
pradeep <pradeep@pradeep-pc>
2014-12-18 11:56:43 +0530

    Transaction Number Generation Formate Is implemented

---
+++ src/main/java/com/shopping/hibernate/service/TransactionNumberGeneration.java
@@ -0,0 +1,49 @@
+package com.shopping.hibernate.service;
+
+import java.util.Calendar;
+
+import org.hibernate.Session;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Property;
+import org.hibernate.criterion.Restrictions;
+
+import com.shopping.hibernate.HibernateUtil;
+import com.shopping.orm.TransactionOrderOrm;
+
+public class TransactionNumberGeneration {
+
+	public  String getCurrentYearDayTime(Integer userid){
+		Calendar date;
+		date = Calendar.getInstance();
+		Integer CurrentDayOfYear=date.get(Calendar.DAY_OF_YEAR);
+		Integer year=date.get(Calendar.YEAR);
+		Integer hour=date.get(Calendar.HOUR);
+		Integer minute=date.get(Calendar.MINUTE);
+		Integer second=date.get(Calendar.SECOND);
+		return year.toString().substring(2,4)+CurrentDayOfYear.toString()+hour.toString()+minute.toString()+second.toString()+userid.toString();
+	}
+
+
+	public String getPerviousnumber(Integer userid){
+		DetachedCriteria maxidoftheinvoicenumber = DetachedCriteria.forClass(TransactionOrderOrm.class)
+				.setProjection( Property.forName("id").max() )
+				.add(Restrictions.like("txnOrderID", this.getCurrentYearDayTime(userid),MatchMode.START));
+
+		Session session =  HibernateUtil.getSessionFactory().openSession();
+
+		try{
+			String previousInvoiceNumber=session.createCriteria(TransactionOrderOrm.class)
+					.add( Property.forName("id").eq(maxidoftheinvoicenumber) )
+					.setProjection(Projections.property("txnOrderID"))
+					.uniqueResult().toString();
+			Integer generatedNumber=Integer.parseInt(previousInvoiceNumber.substring(4, previousInvoiceNumber.length()))+1;
+			
+			return this.getCurrentYearDayTime(userid)+generatedNumber.toString();
+			
+		}catch(Exception exception){
+			return this.getCurrentYearDayTime(userid)+"1";
+		}
+	}
+}
